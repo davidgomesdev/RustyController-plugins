188f3473b3b0a2d9d5dff25fec92d0e523a47c56
David Gomes <LegendL3n@users.noreply.github.com>
2022-10-23 20:17:44 +0100

    refactor(weathery): minor change

--- weathery/effect.py
+++ weathery/effect.py
@@ -20,9 +20,9 @@ EFFECT_BY_TEMP = {
     "LIGHT_RAIN": create_effect(hue=194, saturation=0.84, value=0.04),
     "RAIN": create_effect(hue=220, value=0.04),
     "HEAVY_RAIN": create_effect(hue=240, value=0.04),
-    "A_BIT_CLOUDY": create_effect(hue=26, value=0.1),
+    "A_BIT_CLOUDY": create_effect(hue=35, value=0.05),
     "CLOUDY": create_effect(hue=195, saturation=0.067, value=0.07),
     "THUNDER": create_effect(hue=260, saturation=0.81, value=0.23),
     "SNOW": create_effect(hue=0.0, saturation=0.0, value=0.68),
-    "SUNNY": create_effect(hue=26, value=0.2),
+    "SUNNY": create_effect(hue=35, value=0.17),
 }
