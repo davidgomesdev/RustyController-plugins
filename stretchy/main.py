d1f16cd5fd73a2ed56e45d8abaad3106a6914deb
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-01-13 18:12:17 +0000

    chore(stretchy): change schedule

--- stretchy/main.py
+++ stretchy/main.py
@@ -42,7 +42,7 @@ subscription OnButtonChange {
 }
 """ % STRETCH_ACK_TIMEOUT)
 
-EFFECTIVE_SCHEDULE = [(10, 12), (14, 19)]
+EFFECTIVE_SCHEDULE = [(10, 12), (14, 17)]
 STRETCH_INTERVAL_MINUTES = 5 if is_dev_mode else int(os.environ.get("STRETCH_INTERVAL", "60"))
 
 last_stretch_time = time.time()
